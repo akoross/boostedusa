b839ef697e379f6cfc96b09db3d1a5f9a72d22bf
Sergii <74363342+akoross@users.noreply.github.com>
2021-09-18 23:09:58 +0300

    creeate blok welcome

--- source/js/index.js
+++ source/js/index.js
@@ -14,3 +14,24 @@ if (navIcon) {
     });
   });
 }
+
+const testWebP = (callback) => {
+  const webP = new Image();
+  webP.onload = webP.onerror = () => {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+};
+
+// const result =
+
+// export default result;
+
+testWebP((support) => {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
