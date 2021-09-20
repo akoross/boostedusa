1f50c64e96cc144a133ff30837bfd5e83b7b0fcc
Sergii <74363342+akoross@users.noreply.github.com>
2021-09-20 22:18:46 +0300

    end version 1.0

--- source/js/index.js
+++ source/js/index.js
@@ -1,16 +1,16 @@
-const navIcon = document.querySelector('.nav-header__icon');
-const navHeaderLinks = document.querySelectorAll('.nav-header__link');
+const navIcon = document.querySelector(".nav-header__icon");
+const navHeaderLinks = document.querySelectorAll(".nav-header__link");
 if (navIcon) {
-  const navList = document.querySelector('.nav-header__list');
-  navIcon.addEventListener('click', () => {
-    document.body.classList.toggle('lock');
-    navIcon.classList.toggle('nav-header__icon--active');
-    navList.classList.toggle('nav-header__list--active');
+  const navList = document.querySelector(".nav-header__list");
+  navIcon.addEventListener("click", () => {
+    document.body.classList.toggle("lock");
+    navIcon.classList.toggle("nav-header__icon--active");
+    navList.classList.toggle("nav-header__list--active");
   });
   navHeaderLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-      navIcon.classList.toggle('nav-header__icon--active');
-      navList.classList.toggle('nav-header__list--active');
+    link.addEventListener("click", () => {
+      navIcon.classList.toggle("nav-header__icon--active");
+      navList.classList.toggle("nav-header__list--active");
     });
   });
 }
@@ -21,7 +21,7 @@ const testWebP = (callback) => {
     callback(webP.height == 2);
   };
   webP.src =
-    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 };
 
 // const result =
@@ -30,8 +30,57 @@ const testWebP = (callback) => {
 
 testWebP((support) => {
   if (support == true) {
-    document.querySelector('body').classList.add('webp');
+    document.querySelector("body").classList.add("webp");
   } else {
-    document.querySelector('body').classList.add('no-webp');
+    document.querySelector("body").classList.add("no-webp");
   }
+});
+
+//------------Slider-SWIPER---------------
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  loop: true,
+  // centerInsufficientSlides: true,
+  // centeredSlides: true,
+  // centeredSlides: true,
+  centeredSlidesBounds: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    // when window width is >= 440px
+    440: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 840px
+    840: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1040px
+    1040: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1240px
+    1240: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 7,
+      spaceBetween: 20,
+    },
+  },
 });
